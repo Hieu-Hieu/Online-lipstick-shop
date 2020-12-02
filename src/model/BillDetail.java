64b9a5182be1ad933f287459d32b9e56ba463358
Trung Hieu <50879127+NguyenHieu-NNKHL@users.noreply.github.com>
2020-12-02 13:56:18 +0700

    cap nhat lai model bill and bill detail

--- src/model/BillDetail.java
+++ src/model/BillDetail.java
@@ -1,22 +1,22 @@
 package model;
 
 public class BillDetail {
-	private String billID;
+	private int billID;
 	private String productID;
 	private double price;
 	private int quantity;
 	
-	public BillDetail(String billID, String productID, double price, int quantity) {
+	public BillDetail(int billID, String productID, double price, int quantity) {
 		super();
 		this.billID = billID;
 		this.productID = productID;
 		this.price = price;
 		this.quantity = quantity;
 	}
-	public String getBillID() {
+	public int getBillID() {
 		return billID;
 	}
-	public void setBillID(String billID) {
+	public void setBillID(int billID) {
 		this.billID = billID;
 	}
 	public String getProductID() {
