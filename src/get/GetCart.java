9956d81f506430fd638e4eed93fee6d75c5e88aa
quangdai501 <72977901+quangdai501@users.noreply.github.com>
2020-12-18 14:52:09 +0700

    new

--- src/get/GetCart.java
+++ src/get/GetCart.java
@@ -65,7 +65,6 @@ public class GetCart {
 			cart.add(rs.getInt("quantity"));
 			ListCart.add(cart);
 		}
-		System.out.println(ListCart);
 		return ListCart;
 	}
 
@@ -82,19 +81,6 @@ public class GetCart {
 		return totalCart;
 
 	}
-//	public ArrayList<Cart> getCartByUserID(String userID) throws SQLException {
-//		ArrayList<Cart> ListCart = new ArrayList<Cart>();
-//		PreparedStatement pst = conn.prepareStatement("select * from cart where userID = ?");
-//		ResultSet rs = pst.executeQuery();
-//		while (rs.next()) {
-//			Cart cart = new Cart();
-//			cart.setUserID(rs.getString("userID"));
-//			cart.setProductID(rs.getString("productID"));
-//			cart.setQuantity(rs.getInt("quantity"));
-//			ListCart.add(cart);
-//		}
-//		return ListCart;
-//	}
 
 	public boolean addToCart(int billID, int productID, int quantity) throws SQLException {
 		PreparedStatement pst = conn.prepareStatement(
@@ -112,7 +98,6 @@ public class GetCart {
 		PreparedStatement pst = conn.prepareStatement("select * from bill where userID = ? and paid = false");
 		pst.setInt(1, userID);
 		ResultSet rs = pst.executeQuery();
-		System.out.println("sdfsd");
 		if (rs.next()) {
 			return true;
 		}
